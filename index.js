479146694f8fa075c7821620a42ff2f2a650904d
Sri Prasanna <sriprasanna@users.noreply.github.com>
2021-11-12 11:06:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const os = require('os')
 const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
-  const date = new Date()
-  const cpu = os.cpus().length
-  const availableMem = os.totalmem() / 1024 / 1024
-  const freeMem = os.freemem() / 1024 / 1024
-  const data = `Current time: ${date} <br/> Total CPU: ${cpu} <br/> Total Memory: ${availableMem} <br/> Free Memory: ${freeMem}`
-  
-  res.send(data)
+  res.send('Hey you!')
 })
 
 
